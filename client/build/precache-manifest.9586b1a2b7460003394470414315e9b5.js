fddc97a30d80bc6744fd7551f04971e3f94d00aa
Sachin796 <sachinjaveri.2010@gmail.com>
2020-01-12 16:01:54 -0500

    buildfunctionalities

---
+++ client/build/precache-manifest.9586b1a2b7460003394470414315e9b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/static/media/logo.495d93cc.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e75998645d2fe2db3fc5",
+    "url": "/static/js/main.e746aa04.chunk.js"
+  },
+  {
+    "revision": "d499d87a993599493776",
+    "url": "/static/js/2.1e4a2dbe.chunk.js"
+  },
+  {
+    "revision": "e75998645d2fe2db3fc5",
+    "url": "/static/css/main.e751b889.chunk.css"
+  },
+  {
+    "revision": "23f4c5b36dfcd0c0fe9d2f3ca3283f05",
+    "url": "/index.html"
+  }
+];
