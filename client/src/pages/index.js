290f0b30bf10dab00c042e746fa8b966ee38bdd5
Sachin796 <sachinjaveri.2010@gmail.com>
2020-01-26 19:47:46 -0500

    google analytics

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import NavItems from "../components/nav";
 import Intro from "../components/intro";
 import AboutMe from "../components/aboutme";
@@ -6,25 +6,45 @@ import Contact from "../components/contact";
 import Footer from "../components/footer";
 import Projects from "../components/projects";
 import Techskills from "../components/techskills";
+import ReactGA from "react-ga";
+require("dotenv").config();
+
 // import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import ParticleComponent from "../components/particlejs/particle.js";
 
-function MainPage() {
-  return (
-    <>
-      <Router>
-        <ParticleComponent></ParticleComponent>
-        <NavItems></NavItems>
-        <Intro></Intro>
-        <AboutMe></AboutMe>
-        <Techskills></Techskills>
-        <Projects></Projects>
-        <Contact></Contact>
-        <Footer></Footer>
-      </Router>
-    </>
-  );
+export const initializeReactGA = () => {
+  ReactGA.initialize(process.env.GOOGLE_ANALYTICS_KEYS);
+  ReactGA.pageview("./index.js");
+};
+
+export const logPageView = () => {
+  ReactGA.set({ page: window.location.pathname });
+  ReactGA.pageview(window.location.pathname);
+};
+
+class MainPage extends Component {
+  componentDidMount() {
+    initializeReactGA();
+    logPageView();
+  }
+
+  render() {
+    return (
+      <>
+        <Router>
+          <ParticleComponent></ParticleComponent>
+          <NavItems></NavItems>
+          <Intro></Intro>
+          <AboutMe></AboutMe>
+          <Techskills></Techskills>
+          <Projects></Projects>
+          <Contact></Contact>
+          <Footer></Footer>
+        </Router>
+      </>
+    );
+  }
 }
 
 export default MainPage;
