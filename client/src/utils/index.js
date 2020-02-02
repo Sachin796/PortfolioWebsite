418785ffea22f5294a989e5e8c63c71f2a958555
Sachin796 <sachinjaveri.2010@gmail.com>
2020-02-01 22:45:46 -0500

    Resume changes

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,22 +1,22 @@
-// const axios = require("axios");
+const axios = require("axios");
 
 export default {
   getResume: function() {
-    // const FileSaver = require("file-saver");
-    // axios
-    //   .get("./Resume_Sachin_Jhaveri.pdf", {
-    //     responseType: "arraybuffer",
-    //     headers: {
-    //       Accept: "application/pdf"
-    //     }
-    //   })
-    //   .then(response => {
-    //     console.log(response);
-    //     const blob = new Blob([response.data], {
-    //       type: "application/pdf"
-    //     });
-    //     // response.data is an empty object
-    //     FileSaver.saveAs(blob, Math.random());
-    //   });
+    const FileSaver = require("file-saver");
+    axios
+      .get("./Resume_Sachin_Jhaveri.pdf", {
+        responseType: "arraybuffer",
+        headers: {
+          Accept: "application/pdf"
+        }
+      })
+      .then(response => {
+        console.log(response);
+        const blob = new Blob([response.data], {
+          type: "application/pdf"
+        });
+        // response.data is an empty object
+        FileSaver.saveAs(blob, Math.random());
+      });
   }
 };
