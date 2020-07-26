7f498247b594b75119eec347d0744f659bd35e12
Sachin796 <sachinjaveri.2010@gmail.com>
2020-07-26 02:52:53 -0400

    env setup

--- model/index.js
+++ model/index.js
@@ -6,7 +6,7 @@ var path = require("path");
 var Sequelize = require("sequelize");
 var basename = path.basename(module.filename);
 var env = process.env.NODE_ENV || "development";
-var config = require(__dirname + "/../config/config.json")[env];
+var config = require(__dirname + "/../config/config.js")[env];
 var db = {};
 
 if (config.use_env_variable) {
@@ -16,7 +16,10 @@ if (config.use_env_variable) {
     config.database,
     config.username,
     config.password,
-    config
+    {
+      host: "localhost",
+      dialect: "mysql",
+    }
   );
 }
 
